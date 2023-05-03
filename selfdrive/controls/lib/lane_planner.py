fa2e56a8135aca3d2d8984d1591bbbfc3848d76c
kiwan3819 <119022085+kiwan3819@users.noreply.github.com>
2023-05-03 14:38:28 +0900

    Update lane_planner.py

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -10,8 +10,8 @@ from selfdrive.swaglog import cloudlog
 TRAJECTORY_SIZE = 33
 # camera offset is meters from center car to camera
 if EON:
-  CAMERA_OFFSET = 0.1 #- for right
-  PATH_OFFSET = 0.1
+  CAMERA_OFFSET = 0.2 #- for right
+  PATH_OFFSET = 0.2
 elif TICI:
   CAMERA_OFFSET = 0.04
   PATH_OFFSET = 0.04
